ca20b1e306789ea32031d82914163cdd7785460a
Ujjwal Kumar <154745967+Ujjwal3129@users.noreply.github.com>
2026-08-26 19:45:34 +0530

    Time: 0 ms (100.00%) | Memory: 46.8 MB (30.35%) - LeetSync

--- 142-linked-list-cycle-ii/linked-list-cycle-ii.java
+++ 142-linked-list-cycle-ii/linked-list-cycle-ii.java
@@ -13,16 +13,40 @@ public class Solution {
     public ListNode detectCycle(ListNode head) {
 
 
-        HashSet<ListNode> hs = new HashSet<>();
+        // HashSet<ListNode> hs = new HashSet<>();
 
-        ListNode temp = head;
-        while(temp != null){
-            if(hs.contains(temp)){
+        // ListNode temp = head;
+        // while(temp != null){
+        //     if(hs.contains(temp)){
+        //         return temp;
+        //     }
+
+        //     hs.add(temp);
+        //     temp = temp.next;
+        // }
+
+
+        // return null;
+
+
+        ListNode fast = head;
+        ListNode slow = head;
+
+        while(fast != null && fast.next != null){
+            fast = fast.next.next;
+            slow = slow.next;
+
+            if(fast == slow){
+
+
+                ListNode temp = head;
+
+                while(temp != slow){
+                    temp = temp.next;
+                    slow= slow.next;
+                }
                 return temp;
             }
-
-            hs.add(temp);
-            temp = temp.next;
         }
 
 
