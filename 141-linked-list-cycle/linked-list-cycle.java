04c547af8bcb0b12fd1a02c6c82ee7a821b4835f
Ujjwal Kumar <154745967+Ujjwal3129@users.noreply.github.com>
2026-08-26 19:11:48 +0530

    Time: 0 ms (100.00%) | Memory: 46.7 MB (73.19%) - LeetSync

--- 141-linked-list-cycle/linked-list-cycle.java
+++ 141-linked-list-cycle/linked-list-cycle.java
@@ -14,26 +14,45 @@ public class Solution {
         HashSet<ListNode> hs = new HashSet<>();
 
 
-        ListNode temp = head;
+        // ListNode temp = head;
 
 
-        while(temp!=null){
+        // while(temp!=null){
 
-             if(hs.contains(temp)){
-                return true;
-            }
-            hs.add(temp);
+        //      if(hs.contains(temp)){
+        //         return true;
+        //     }
+        //     hs.add(temp);
 
 
             
 
 
-            temp = temp.next;
-            // temp=temp.next;
-        } 
+        //     temp = temp.next;
+        
+        // } 
 
 
       
+
+
+        // return false;
+
+
+
+        ListNode fast = head;
+        ListNode slow = head;
+
+
+        while(fast != null && fast.next !=null){
+            fast = fast.next.next;
+            slow = slow.next;
+
+
+            if(fast == slow){
+                return true;
+            }
+        }
 
 
         return false;
