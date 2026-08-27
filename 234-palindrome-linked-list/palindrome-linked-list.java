02607534499432cd9a018077f1df349d755d4005
Ujjwal Kumar <154745967+Ujjwal3129@users.noreply.github.com>
2026-08-27 11:50:43 +0530

    Time: 3 ms (99.82%) | Memory: 94 MB (96.11%) - LeetSync

--- 234-palindrome-linked-list/palindrome-linked-list.java
+++ 234-palindrome-linked-list/palindrome-linked-list.java
@@ -37,29 +37,93 @@ class Solution {
         //     return true;
 
 
-        Stack<ListNode> st = new Stack<>();
+        // Stack<ListNode> st = new Stack<>();
 
-        ListNode temp = head;
+        // ListNode temp = head;
 
-        while(temp!=null){
-            st.push(temp);
-            temp = temp.next;
+        // while(temp!=null){
+        //     st.push(temp);
+        //     temp = temp.next;
+        // }
+
+
+
+        // int left = 0;
+        // int right = st.size()-1;
+
+        // while(left<right){
+        //     if(st.get(left).val != st.get(right).val){
+        //         return false;
+        //     }
+        //     left++;
+        //     right--;
+        // }
+
+        // return true;
+
+
+              if(head == null && head.next == null){
+            return true;
+        }
+        ListNode fast = head;
+        ListNode slow = head;
+
+
+        while(fast != null && fast.next != null){
+            fast = fast.next.next;
+            slow = slow.next;
+           
         }
 
 
+        if(fast!=null){
+            slow = slow.next;
 
-        int left = 0;
-        int right = st.size()-1;
+        }
 
-        while(left<right){
-            if(st.get(left).val != st.get(right).val){
+
+        ListNode prev = null;
+        ListNode curr = slow;
+
+
+        while(curr != null){
+            ListNode Next = curr.next;
+
+            curr.next = prev;
+            prev = curr;
+
+            curr=Next;
+
+        }
+
+
+        ListNode first = head;
+        ListNode second = prev;
+
+
+        while(second != null){
+            if(first.val != second.val){
                 return false;
             }
-            left++;
-            right--;
+
+            first = first.next;
+            second = second.next;
         }
 
+
         return true;
+
+
+
+
+
+
+
+
+
+
+
+
 
 
         
