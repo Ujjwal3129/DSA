dddf457ba2263de5799f687bb8063da59ef3de3f
Ujjwal Kumar <154745967+Ujjwal3129@users.noreply.github.com>
2026-08-27 11:16:08 +0530

    Time: 10 ms (14.01%) | Memory: 101.6 MB (8.14%) - LeetSync

--- 234-palindrome-linked-list/palindrome-linked-list.java
+++ 234-palindrome-linked-list/palindrome-linked-list.java
@@ -11,31 +11,55 @@
 class Solution {
     public boolean isPalindrome(ListNode head) {
 
-        ArrayList<ListNode> list = new ArrayList<>();
+        // ArrayList<ListNode> list = new ArrayList<>();
 
-        ListNode temp = head;
-        while(temp != null){
-           list.add(temp);
-           temp = temp.next;
+        // ListNode temp = head;
+        // while(temp != null){
+        //    list.add(temp);
+        //    temp = temp.next;
 
-            }
+        //     }
 
 
-            int left = 0;
-            int right = list.size()-1;
-            while(left<right){
-                if(list.get(right).val !=list.get(left).val){
-                      return false;
-                }
+        //     int left = 0;
+        //     int right = list.size()-1;
+        //     while(left<right){
+        //         if(list.get(right).val !=list.get(left).val){
+        //               return false;
+        //         }
              
 
-                 left++;
-                 right--;
+        //          left++;
+        //          right--;
+        //     }
+
+
+        //     return true;
+
+
+        Stack<ListNode> st = new Stack<>();
+
+        ListNode temp = head;
+
+        while(temp!=null){
+            st.push(temp);
+            temp = temp.next;
+        }
+
+
+
+        int left = 0;
+        int right = st.size()-1;
+
+        while(left<right){
+            if(st.get(left).val != st.get(right).val){
+                return false;
             }
+            left++;
+            right--;
+        }
 
-
-            return true;
-        
+        return true;
 
 
         
