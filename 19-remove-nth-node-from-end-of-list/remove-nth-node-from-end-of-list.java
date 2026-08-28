3fd07dada23f2ea43fef3b3c0b056c315ec7513b
Ujjwal Kumar <154745967+Ujjwal3129@users.noreply.github.com>
2026-08-28 19:38:27 +0530

    Time: 0 ms (100.00%) | Memory: 43.6 MB (40.14%) - LeetSync

--- 19-remove-nth-node-from-end-of-list/remove-nth-node-from-end-of-list.java
+++ 19-remove-nth-node-from-end-of-list/remove-nth-node-from-end-of-list.java
@@ -15,50 +15,31 @@ class Solution {
 
         ListNode temp = head;
 
-
-        while(temp!=null){
+        while (temp != null) {
             list.add(temp);
             temp = temp.next;
         }
 
-
         int count = 0;
 
         if (n == list.size()) {
-    return head.next;
-}
-
-        ListNode prev= null;
-        for(int i=list.size()-1; i>=0; i--){
-            count++;
-            if(n==count){
-
-
-                  list.get(i-1).next = list.get(i).next;
-                  continue;
-
-
-            }
-
-            // prev= i;
-
-            // list.add(list.get(i))
-                //   list.get(i) = list.get(i).next;
-
-
-            
-
-
-          
+            return head.next;
         }
 
 
+        for (int i = list.size() - 1; i >= 0; i--) {
+            count++;
+            if (n == count) {
+
+                list.get(i - 1).next = list.get(i).next;
+                // continue;
+
+            }
+
+
+        }
+
         return head;
-
-
-
-
-        
 
     }
 }
