5eb821a682e878fdade50a897de87d9648dab9be
Ujjwal Kumar <154745967+Ujjwal3129@users.noreply.github.com>
2026-09-11 22:49:37 +0530

    Time: 11 ms (19.47%) | Memory: 47.3 MB (7.47%) - LeetSync

---
+++ 3799-unique-3-digit-even-numbers/unique-3-digit-even-numbers.java
@@ -0,0 +1,41 @@
+class Solution {
+    public int totalNumbers(int[] digits) {
+        int[] count = new int[10];
+
+        for (int digit : digits) {
+            count[digit]++;
+        }
+
+        int ans = 0;
+
+        for (int num = 100; num <= 999; num++) {
+            if (num % 2 != 0) {
+                continue;
+            }
+
+            int a = num / 100;
+            int b = (num / 10) % 10;
+            int c = num % 10;
+
+            int[] used = new int[10];
+            used[a]++;
+            used[b]++;
+            used[c]++;
+
+            boolean possible = true;
+
+            for (int i = 0; i < 10; i++) {
+                if (used[i] > count[i]) {
+                    possible = false;
+                    break;
+                }
+            }
+
+            if (possible) {
+                ans++;
+            }
+        }
+
+        return ans;
+    }
+}
