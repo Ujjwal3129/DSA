78c000ad6f6194af28cfccc3b8a6a99ecef02079
Ujjwal Kumar <154745967+Ujjwal3129@users.noreply.github.com>
2026-09-16 22:10:44 +0530

    Time: 25 ms (77.42%) | Memory: 63.2 MB (63.87%) - LeetSync

---
+++ 1725-number-of-sets-of-k-non-overlapping-line-segments/number-of-sets-of-k-non-overlapping-line-segments.java
@@ -0,0 +1,28 @@
+class Solution {
+    public int numberOfSets(int n, int k) {
+        final long MOD = 1_000_000_007L;
+
+        long[][] dp = new long[k + 1][n];
+
+        // 0 segments: 1 way
+        for (int i = 0; i < n; i++) {
+            dp[0][i] = 1;
+        }
+
+        for (int seg = 1; seg <= k; seg++) {
+            long sum = 0;
+
+            for (int i = 1; i < n; i++) {
+                /*
+                 * Start a segment at some previous point.
+                 * Number of choices can be accumulated using prefix sums.
+                 */
+                sum = (sum + dp[seg - 1][i - 1]) % MOD;
+
+                dp[seg][i] = (dp[seg][i - 1] + sum) % MOD;
+            }
+        }
+
+        return (int) dp[k][n - 1];
+    }
+}
